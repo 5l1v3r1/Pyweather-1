7f5cff69cec55ff205a76c9f1a373c95a46113c9
Sameer ahmad khan <48324603+thetimeloops@users.noreply.github.com>
2019-07-21 01:04:49 +0530

    more features to come

---
+++ weth.py
@@ -0,0 +1,24 @@
+import requests 
+import bs4
+from colorama import *
+init(autoreset = True)
+while True:
+	location = input("Input  any Country/State/City to get weather info >  ")
+	header = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"}
+	rq = requests.get("http://www.google.com/search",params={'q':f"{location} weather"},headers=header)
+	soup = bs4.BeautifulSoup(rq.text , "html.parser")
+	try:
+		loc = soup.find('div' , {'id' : 'wob_loc'})
+		temp = soup.find('span' , {'id' : 'wob_tm'})
+		time = soup.find('div' , {'id' : 'wob_dts'})
+		stat = soup.find('div' , {'id' : 'wob_dcp'})
+		print("\n")
+		print("LOCATION : " +loc.text)
+		print("TEMPRATURE : "+temp.text)
+		print("WEATHER UPDATED ON : " +time.text)
+		print(stat.text)
+		print("\n")
+		
+	except:
+		print(Back.RED +f"{location} is not a valid location ")
+		print("\n")
